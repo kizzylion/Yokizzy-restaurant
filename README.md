ae16e1a18011010c4ea40c3d79651c05008c71dd
kizzylion <kztchm@gmail.com>
2024-02-02 07:03:41 -0500

    Update production build

--- README.md
+++
@@ -1,1 +0,0 @@
-# Yokizzy-restaurant
--- src/welcomebadge.js
+++
@@ -1,8 +0,0 @@
-const closeBadge = document.getElementById('badgeClose');
-
-const welcomeImg = document.getElementById('welcomeImg');
-const welcomeBox = document.getElementById('box');
-
-closeBadge.addEventListener("click", function() {
-    welcomeBox.classList.toggle('hide');
-})
