65c5abd63e7c5d821faf3fec2429589c2919faca
kizzylion <kztchm@gmail.com>
2024-02-01 19:56:31 -0500

    Dist meta info

--- webpack.common.js
+++ webpack.common.js
@@ -25,11 +25,12 @@ module.exports = {
             }
           },
           {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
             type: 'asset/resource',
             generator: {
               filename: 'images/[name][ext]',
             },
+            
           },
           {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
