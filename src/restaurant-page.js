ddf1e0a21411a7280605810cb18696907c01c85b
kizzylion <kztchm@gmail.com>
2024-02-03 05:55:11 -0500

    FIX Homepage Carousel with js

--- src/restaurant-page.js
+++ src/restaurant-page.js
@@ -59,19 +59,19 @@ export default function createRestaurantHomepage(){
     })
 
     // Function to move carousel
-    // function moveCarousel() {
-    //     const firstItem = carousel.firstElementChild;
-    //     carousel.appendChild(firstItem.cloneNode(true));
-    //     carousel.removeChild(firstItem);
-    //     carousel.scrollTo({
-    //         left: carousel.scrollLeft + firstItem.offsetWidth, // scroll one item width
-    //         behavior: 'smooth', // smooth scroll
-    //     });
+    function moveCarousel() {
+        const firstItem = carousel.firstElementChild;
+        carousel.appendChild(firstItem.cloneNode(true));
+        carousel.removeChild(firstItem);
+        carousel.scrollTo({
+            left: carousel.scrollLeft + firstItem.offsetWidth, // scroll one item width
+            behavior: 'smooth', // smooth scroll
+        });
 
-    // }
+    }
 
-    // // Set interval to move carousel every 3 seconds
-    // setInterval(moveCarousel, 2000);
+    // Set interval to move carousel every 3 seconds
+    setInterval(moveCarousel, 2000);
 
 }
 
